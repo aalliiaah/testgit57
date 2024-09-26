a186f4758d36225325a166aad4ae9a1848efbd22
aalliiaah <aliah0909abdulrhman@gmail.com>
2024-09-26 11:49:23 +0300

    Initial Commit

---
+++ testgit57/testgit57App.swift
@@ -0,0 +1,17 @@
+//
+//  testgit57App.swift
+//  testgit57
+//
+//  Created by Aliah Alhameed on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct testgit57App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
